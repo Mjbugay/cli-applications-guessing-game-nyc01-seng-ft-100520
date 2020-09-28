42cafad96f5d7ebf215805719e506e0160e83db6
Matt Bugay <mjbugay@yahoo.com>
2020-09-28 00:04:32 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,8 +6,7 @@ def run_guessing_game
     
     if guess == "exit"
       puts "Goodbye!"
-      break
-      
+  
     elsif guess.to_i == computer_number
       puts "You guessed the correct number!"
         
